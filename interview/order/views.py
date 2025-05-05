73731234c6476294db7e716e7a06103c9bf0567a
Jason Nguyen <jadoking@yu-yu.local>
2025-05-05 15:54:50 -0700

    added tags list view

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from interview.order.models import Order
+from interview.order.serializers import OrderTagSerializer
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
@@ -13,3 +17,15 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class OrderTagsListView(APIView):
+    def get(self, request: Request, order_id: int) -> Response:
+        try:
+            order = Order.objects.prefetch_related('tags').get(id=order_id)
+        except Order.DoesNotExist:
+            return Response({'error': 'Order not found'}, status=404)
+
+        tags = order.tags.all()
+        serializer = OrderTagSerializer(tags, many=True)
+        return Response(serializer.data, status=200) 
