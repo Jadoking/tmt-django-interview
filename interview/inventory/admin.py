ea38e70adbe1dbff78d3a2e9c9de41d6a32f92fb
Jason Nguyen <jadoking@yu-yu.local>
2025-05-05 15:49:37 -0700

    add admin for inventory and order

--- interview/inventory/admin.py
+++ interview/inventory/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from interview.inventory.models import Inventory, InventoryType, InventoryLanguage, InventoryTag
 
-# Register your models here.
+
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+    list_filter = ('type', 'language')
+    filter_horizontal = ('tags',)
+
+
+@admin.register(InventoryType)
+class InventoryTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(InventoryLanguage)
+class InventoryLanguageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(InventoryTag)
+class InventoryTagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_active')
+    list_filter = ('is_active',)
