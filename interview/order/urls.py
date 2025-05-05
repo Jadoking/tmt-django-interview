abc93ee0f87531c31fe41ae9607c66fc1baad4c5
Jason Nguyen <jadoking@yu-yu.local>
2025-05-05 15:13:13 -0700

    init commit add embargo-in-range

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import OrderListCreateView, OrderTagListCreateView, OrderInDateRangeView
 
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
+    path('embargo-in-range/', OrderInDateRangeView.as_view(), name='order-in-range'),
     path('', OrderListCreateView.as_view(), name='order-list'),
 
 ]
